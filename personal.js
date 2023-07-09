1dc8585b9d2806e01ca8605d813b8c6e037c9fae
Arul-Murugan-S <arulmurugansv2@gmail.com>
2023-07-09 09:45:20 +0530

    not include in .gitignore at main

---
+++ personal.js
@@ -0,0 +1,3 @@
+console.log("Hello from Node!");
+
+This personal.js is not added by .gitignore method in main branch but it has added by .gitignore from GitIgnore branch. So personal.js will appear in main branch but it will not appear in GitIgnore branch.
